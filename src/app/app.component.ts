8ab3cde33d63ed175b61282bb38c4f3785367fed
Igor <lomakinigor96@gmail.com>
2022-08-30 15:30:34 +0300

    Add frontend with cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Car } from './interface/car';
+import { CarService } from './car.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rentapp';
+export class AppComponent implements OnInit{
+  public cars: Car[] = [];
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void{
+    this.getCars();
+  }
+  
+  public getCars(): void{
+    this.carService.getCars().subscribe(
+      (response: Car[]) => {
+        this.cars = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarService } from './car.service';
+import { CarListComponent } from './car-list/car-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
